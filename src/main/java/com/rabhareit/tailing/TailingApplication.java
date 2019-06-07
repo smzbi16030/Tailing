99f5df79ad0fd9e9a5d5b16394297c5a1a331b92
smzbi16030 <yusuke43z@outlook.com>
2019-06-07 22:19:06 +0900

    deploy test

--- src/main/java/com/rabhareit/tailing/TailingApplication.java
+++ src/main/java/com/rabhareit/tailing/TailingApplication.java
@@ -3,7 +3,9 @@ package com.rabhareit.tailing;
 import com.rabhareit.tailing.properties.FirebaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
@@ -18,11 +20,15 @@ import org.springframework.context.annotation.PropertySources;
     //FirebaseProperties.class,
     //SocialConfiguration.class
 //})
-public class TailingApplication {
+public class TailingApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(TailingApplication.class, args);
+  }
 
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(TailingApplication.class);
   }
 
 }
