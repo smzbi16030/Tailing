4805523212aed32f35a1ffa7e7cb7ad42826571b
smzbi16030 <yusuke43z@outlook.com>
2019-06-08 04:55:35 +0900

    deploy

--- WEB-INF/classes/static/js/completeButton.js
+++
@@ -1,8 +0,0 @@
-function compCheck(){
-	if(window.confirm('このタスクを完了し、アーカイブします。')){ // 確認ダイアログを表示
-		return true; // 「OK」時は送信を実行
-	}
-	else{ // 「キャンセル」時の処理
-		return false; // 送信を中止
-	}
-}
--- WEB-INF/classes/static/js/inputStyle.js
+++
@@ -1,3 +0,0 @@
-$('.textbox input').attr('value', "")
-$('.textbox input').attr('onkeyup', "this.setAttribute('value', this.value);")
-$('.textbox .info').append('<i class="material-icons">info</i>')
