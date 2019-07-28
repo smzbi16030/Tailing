04eab9f7c026f22d18be28ae4f454f83d34b55d8
Yusuke Shimizu <yusuk@bi16030PC.local>
2019-07-28 16:59:49 +0900

    heroku commit

--- src/main/java/com/rabhareit/tailing/TailingApplication.java
+++ src/main/java/com/rabhareit/tailing/TailingApplication.java
@@ -2,13 +2,20 @@ package com.rabhareit.tailing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.io.IOException;
 
 @SpringBootApplication
-public class TailingApplication {
+public class TailingApplication extends SpringBootServletInitializer {
   public static void main(String[] args) throws IOException {
     SpringApplication.run(TailingApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(TailingApplication.class);
   }
 
 }
