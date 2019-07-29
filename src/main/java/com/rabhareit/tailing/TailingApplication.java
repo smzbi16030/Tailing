e3f36a61f60289cdcfabdc5dd48772bc9bb37806
rabhare6it <yusuke43z@outlook.com>
2019-07-29 23:51:57 +0900

    Error Handling

--- src/main/java/com/rabhareit/tailing/TailingApplication.java
+++ src/main/java/com/rabhareit/tailing/TailingApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class TailingApplication extends SpringBootServletInitializer {
   public static void main(String[] args) {
