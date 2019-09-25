67e25d31270e6adfccee2cd5b5c9364198142180
rabhare6it <yusuke43z@outlook.com>
2019-09-26 00:46:19 +0900

    Profile image URL improvement.

--- src/main/java/com/rabhareit/tailing/service/ConnectionSignUpImpl.java
+++ src/main/java/com/rabhareit/tailing/service/ConnectionSignUpImpl.java
@@ -77,8 +77,8 @@ public class ConnectionSignUpImpl implements ConnectionSignUp {
     account.setScreenName(user.getName());
     account.setUserName(user.getScreenName());
     account.setTwitterId(user.getId());
-    account.setImageUrl(user.getProfileImageURL());
-    account.setBannerUrl(user.getProfileBannerURL());
+    account.setImageUrl(user.getOriginalProfileImageURLHttps());
+    account.setBannerUrl(user.getProfileBannerURL() + "_retina");
     return account;
   }
 
